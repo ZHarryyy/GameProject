2e7c44a6cf3b7e79939aae1b1433fdc497572b3b
Harry <zharryyyyy@gmail.com>
2023-12-14 22:22:24 +0800

    完善关卡设计，实现角色和子弹朝向

--- Assets/Codes/Project/Player.cs
+++ Assets/Codes/Project/Player.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,12 +9,18 @@ namespace PlatformShoot
         private Rigidbody2D mRig;
         private float mGroundMoveSpeed = 5f;
         private float mJumpForce = 12f;
+
         private bool mJumpInput;
+        private int mFaceDir = 1;
+
         private MainPanel mMainPanel;
+        private GameObject mGamePass;
 
         private void Start()
         {
             mRig = GetComponent<Rigidbody2D>();
+            mGamePass = GameObject.Find("GamePass");
+            mGamePass.SetActive(false);
             mMainPanel = GameObject.Find("MainPanel").GetComponent<MainPanel>();
         }
 
@@ -21,8 +28,11 @@ namespace PlatformShoot
         {
             if(Input.GetKeyDown(KeyCode.J))
             {
-                var bullet = Resources.Load<GameObject>("Bullet");
-                GameObject.Instantiate(bullet, transform.position, Quaternion.identity);
+                var obj = Resources.Load<GameObject>("Bullet");
+                obj = GameObject.Instantiate(obj, transform.position, Quaternion.identity);
+                Bullet bullet = obj.GetComponent<Bullet>();
+                bullet.GetGamePass(mGamePass);
+                bullet.InitDir(mFaceDir);
             }
             if(Input.GetKeyDown(KeyCode.K))
             {
@@ -37,7 +47,13 @@ namespace PlatformShoot
                 mJumpInput = false;
                 mRig.velocity = new Vector2(mRig.velocity.x, mJumpForce);
             }
-            mRig.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * mGroundMoveSpeed, mRig.velocity.y);
+            float h = Input.GetAxisRaw("Horizontal");
+            if(h != 0 && h != mFaceDir)
+            {
+                mFaceDir = -mFaceDir;
+                transform.Rotate(0, 180, 0);
+            }
+            mRig.velocity = new Vector2(h * mGroundMoveSpeed, mRig.velocity.y);
         }
 
         private void OnTriggerEnter2D(Collider2D coll)
